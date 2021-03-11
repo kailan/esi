dc28acb91191c13c2f322224482117d5670722ab
Kailan Blanks <kailan@enviark.com>
2021-03-11 23:59:57 +0000

    Implement struct-based parsing and parameter parsing

--- esi/src/lib.rs
+++ esi/src/lib.rs
@@ -1,14 +1,19 @@
 #[macro_use]
 extern crate lazy_static;
 
-use fancy_regex::Regex;
+use std::collections::{HashMap, hash_map::Entry};
+
+use fancy_regex::{Captures, Regex};
 
 lazy_static! {
     // Self-enclosed tags, such as <esi:comment text="Just write some HTML instead"/>
-    static ref EMPTY_TAG_REGEX: Regex = Regex::new(r"(?si)<esi:(?P<tag>[A-z]+)(?P<attributes>.*)/>").unwrap();
+    static ref EMPTY_TAG_REGEX: Regex = Regex::new(r"(?si)\s*<esi:(?P<tag>[A-z]+)(?P<parameters>.*?)/>\s*").unwrap();
 
     // Tags with content, such as <esi:remove>test</esi:remove>
-    static ref CONTENT_TAG_REGEX: Regex = Regex::new(r"(?si)<esi:(?P<tag>[A-z]+)(?P<attributes>.*)>(.*)</esi:(?P=tag)+>").unwrap();
+    static ref CONTENT_TAG_REGEX: Regex = Regex::new(r"(?si)\s*<esi:(?P<tag>[A-z]+)(?P<parameters>.*?)>(?P<content>.+)</esi:(?P=tag)+>\s*").unwrap();
+
+    // Parameters, e.g. data="test"
+    static ref PARAMETER_REGEX: Regex = Regex::new(r#"\s*(.+?)="(.*?)""#).unwrap();
 }
 
 pub struct Error {
@@ -29,44 +34,91 @@ pub trait RequestHandler {
 }
 
 pub fn transform_esi_string(mut body: String, client: &impl RequestHandler) -> Result<String, Error> {
-    body = execute_empty_tags(body, client)?;
     body = execute_content_tags(body, client)?;
+    body = execute_empty_tags(body, client)?;
 
     println!("done.");
 
     Ok(body)
 }
 
-fn execute_empty_tags(body: String, client: &impl RequestHandler) -> Result<String, Error> {
+#[derive(Debug)]
+pub struct Tag {
+    name: String, // "include"
+    content: Option<String>, // "hello world"
+    parameters: HashMap<String, String> // src = "/a.html"
+}
+
+impl Tag {
+    fn empty_from_captures(cap: Captures) -> Tag {
+        Tag {
+            name: cap.name("tag").unwrap().as_str().to_string(),
+            content: None,
+            parameters: Tag::parse_parameters(cap.name("parameters").unwrap().as_str())
+        }
+    }
+
+    fn with_content_from_captures(cap: Captures) -> Tag {
+        Tag {
+            name: cap.name("tag").unwrap().as_str().to_string(),
+            content: Some(cap.name("content").unwrap().as_str().to_string()),
+            parameters: Tag::parse_parameters(cap.name("parameters").unwrap().as_str())
+        }
+    }
+
+    fn parse_parameters(input: &str) -> HashMap<String, String> {
+        let mut map = HashMap::new();
+
+        for cap in PARAMETER_REGEX.captures_iter(input) {
+            match cap {
+                Ok(cap) => {
+                    map.insert(String::from(cap.get(1).unwrap().as_str()), String::from(cap.get(2).unwrap().as_str()));
+                },
+                _ => {}
+            }
+        }
+
+        map
+    }
+}
+
+fn execute_empty_tags(mut body: String, client: &impl RequestHandler) -> Result<String, Error> {
     let element = EMPTY_TAG_REGEX.find(&body).unwrap_or_default();
 
     match element {
         Some(element) => {
-            println!("executing {}", element.as_str());
+            let tag = Tag::empty_from_captures(EMPTY_TAG_REGEX.captures(&body).unwrap().unwrap());
 
-            let tag = EMPTY_TAG_REGEX.captures(&body).unwrap().unwrap().name("tag").unwrap().as_str();
-            if tag == "include" {
-                execute_empty_tags(body.replace(element.as_str(), "<span>include</span>"), client)
+            println!("{:?}", tag);
+
+            if tag.name == "include" {
+                body = body.replace(element.as_str(), "<span>include</span>");
+                execute_empty_tags(body, client)
+            } else if tag.name == "comment" {
+                body = body.replace(element.as_str(), "");
+                execute_empty_tags(body, client)
             } else {
-                Err(Error::from_message(&format!("Unsupported tag: <esi:{}>", tag)))
+                Err(Error::from_message(&format!("Unsupported tag: <esi:{}>", tag.name)))
             }
         },
         None => Ok(body)
     }
 }
 
-fn execute_content_tags(body: String, client: &impl RequestHandler) -> Result<String, Error> {
+fn execute_content_tags(mut body: String, client: &impl RequestHandler) -> Result<String, Error> {
     let element = CONTENT_TAG_REGEX.find(&body).unwrap_or_default();
 
     match element {
         Some(element) => {
-            println!("executing {}", element.as_str());
+            let tag = Tag::with_content_from_captures(CONTENT_TAG_REGEX.captures(&body).unwrap().unwrap());
 
-            let tag = CONTENT_TAG_REGEX.captures(&body).unwrap().unwrap().name("tag").unwrap().as_str();
-            if tag == "remove" {
-                execute_empty_tags(body.replace(element.as_str(), ""), client)
+            println!("{:?}", tag);
+
+            if tag.name == "remove" {
+                body = body.replace(element.as_str(), "");
+                execute_content_tags(body, client)
             } else {
-                Err(Error::from_message(&format!("Unsupported tag: <esi:{}>", tag)))
+                Err(Error::from_message(&format!("Unsupported tag: <esi:{}>", tag.name)))
             }
         },
         None => Ok(body)
